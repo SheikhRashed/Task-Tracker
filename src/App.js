9822ebb04547c346e4c7e123b11dae66aff8fa04
Sheikh Rashed <mdrashed1310@gmail.com>
2021-03-31 16:57:14 +0600

    Global State

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 
 // components
 import Header from './components/Header';
 import TaskList from './components/Tasks';
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting with clients',
+      day: 'Feb 30th at 5:00pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Interview at Lotus technology Development ',
+      day: 'April 01 at 3:30pm',
+      reminder: true,
+    },
+  ]);
   return (
     <div className='container'>
       <Header />
-      <TaskList />
+      <TaskList tasks={tasks} />
     </div>
   );
 }
